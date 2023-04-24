6d566cb250e36372a9a7ab0caf67ab5e33220878
bwiss11 <bwiss11@gmail.com>
2023-04-23 23:04:05 -0400

    Dynamically looking up player stats using their name which is converted tot their playerid.

--- js/test.mjs
+++ js/test.mjs
@@ -17,33 +17,87 @@ async function getUsers() {
 // }
 
 //getMLB().then(data => console.log(data.people[0].stats[0].splits[0].stat.avg, data.people[0].stats[0].splits[0].stat))
+//https://statsapi.mlb.com/api/v1/stats?stats=sabermetrics&group=hitting&sportId=1&season=2023
+
+// async function get2023Players() {
+//     let response = await fetch("https://statsapi.mlb.com/api/v1/stats?stats=sabermetrics&group=hitting&sportId=1&season=2023");
+//     let data = await response.json()
+//     return data
+// }
+
+
+// for (const person in players2023.stats[0].splits) {
+//     console.log(person)
+// };
+// console.log(players2023.stats[0].splits)
+// console.log(players2023.stats[0].splits[0].player.fullName)
+// console.log(players2023.stats[0].splits.slice(-1)[0].player.fullName)
+// console.log('players above')
+
+// async function get2023PlayerStats() {
+//     let response = await fetch("https://statsapi.mlb.com/api/v1/people?personIds=668939&hydrate=stats(group=[batting],type=[yearByYear])");
+//     let data = await response.json()
+//     return data.people[0]//.stats[0].splits.slice(-1)[0].stat;
+// }
+
+
+async function get2023Players() {
+     let response = await fetch("https://statsapi.mlb.com/api/v1/sports/1/players?season=2023");
+     let data = await response.json()
+     console.log(data.people[0], 'new')
+     return data.people
+};
+
+let players2023 = await get2023Players()
+
+// Creates two dicts
+var idToNameDict = new Object();
+var nameToIdDict = new Object();
+
+// first fills out dict from id:name (doing name:id first missed a few entries for some reason)
+for (let i = 0; i < players2023.length; i++) {
+    console.log(players2023[i])
+    idToNameDict[players2023[i].id] = players2023[i].fullName
+
+};
+
+for (const [key, value] of Object.entries(idToNameDict)) {
+    nameToIdDict[value] = key;
+  }
+
+// Converts to name: id format
+// console.log(nameToIdDict, 'name to id')
+
+
+
+let playerId = nameToIdDict['Mike Trout']
 
 
 async function get2023PlayerStats() {
-    let response = await fetch("https://statsapi.mlb.com/api/v1/people?personIds=668939&hydrate=stats(group=[batting],type=[yearByYear])");
+    let response = await fetch("https://statsapi.mlb.com/api/v1/people?personIds=" + playerId + "&hydrate=stats(group=[batting],type=[yearByYear])");
     let data = await response.json()
-    console.log(data.people[0].fullName)
     return data.people[0]//.stats[0].splits.slice(-1)[0].stat;
 }
 
+let playerStats = await get2023PlayerStats()
+
+console.log(playerStats, 'player stats')
+
+
 //get2023PlayerStats().then(data => console.log(data))
 //getMLBAll().then(data => console.log(data.people[0].stats[0].splits.slice(-1)))
-let stats2023 = await get2023PlayerStats()
-console.log(stats2023)
-let name = stats2023.fullName
-let hits = stats2023.stats[0].splits.slice(-1)[0].stat.hits
-let avg = stats2023.stats[0].splits.slice(-1)[0].stat.avg
-let obp = stats2023.stats[0].splits.slice(-1)[0].stat.obp
-let slg = stats2023.stats[0].splits.slice(-1)[0].stat.slg
-let rbis = stats2023.stats[0].splits.slice(-1)[0].stat.rbi
-let hrs = stats2023.stats[0].splits.slice(-1)[0].stat.homeRuns
-let ops = stats2023.stats[0].splits.slice(-1)[0].stat.ops
-// let ops = (parseInt(obp) + parseInt(slg)).toString()
+// let stats2023 = await get2023PlayerStats()
+let name = playerStats.fullName
+let hits =  playerStats.stats[0].splits.slice(-1)[0].stat.hits
+let avg = playerStats.stats[0].splits.slice(-1)[0].stat.avg
+let obp = playerStats.stats[0].splits.slice(-1)[0].stat.obp
+let slg = playerStats.stats[0].splits.slice(-1)[0].stat.slg
+let rbis = playerStats.stats[0].splits.slice(-1)[0].stat.rbi
+let hrs = playerStats.stats[0].splits.slice(-1)[0].stat.homeRuns
+let ops = playerStats.stats[0].splits.slice(-1)[0].stat.ops
 
+console.log(avg, obp, slg, rbis, hrs, ops)
 
-console.log(stats2023)
-
-console.log(hits, avg, obp, slg, rbis)
 document.getElementById("name").innerHTML = name
 document.getElementById("avg").innerHTML = avg
 document.getElementById("hits").innerHTML = hits
@@ -52,5 +106,5 @@ document.getElementById("slg").innerHTML = slg
 document.getElementById("rbis").innerHTML = rbis
 document.getElementById("hrs").innerHTML = hrs
 document.getElementById("ops").innerHTML = ops
-export {get2023PlayerStats};
+// export {get2023PlayerStats};
 
