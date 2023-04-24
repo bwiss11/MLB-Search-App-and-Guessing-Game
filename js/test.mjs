6c81f0e450713ab8531e12df11c7947328b976c1
bwiss11 <bwiss11@gmail.com>
2023-04-24 17:48:15 -0400

    Pulling stats and headshots for all Orioles batters (hardcoded names).

--- js/test.mjs
+++ js/test.mjs
@@ -56,7 +56,6 @@ var nameToIdDict = new Object();
 
 // first fills out dict from id:name (doing name:id first missed a few entries for some reason)
 for (let i = 0; i < players2023.length; i++) {
-    console.log(players2023[i])
     idToNameDict[players2023[i].id] = players2023[i].fullName
 
 };
@@ -69,8 +68,8 @@ for (const [key, value] of Object.entries(idToNameDict)) {
 // console.log(nameToIdDict, 'name to id')
 
 
-
-let playerId = nameToIdDict['Mike Trout']
+let inputPlayer = 'Adam Frazier'
+let playerId = nameToIdDict[inputPlayer]
 
 
 async function get2023PlayerStats() {
@@ -81,7 +80,6 @@ async function get2023PlayerStats() {
 
 let playerStats = await get2023PlayerStats()
 
-console.log(playerStats, 'player stats')
 
 
 //get2023PlayerStats().then(data => console.log(data))
@@ -96,7 +94,10 @@ let rbis = playerStats.stats[0].splits.slice(-1)[0].stat.rbi
 let hrs = playerStats.stats[0].splits.slice(-1)[0].stat.homeRuns
 let ops = playerStats.stats[0].splits.slice(-1)[0].stat.ops
 
-console.log(avg, obp, slg, rbis, hrs, ops)
+let position = playerStats.primaryPosition.abbreviation
+let team = playerStats.stats[0].splits.slice(-1)[0].team.name
+let number = playerStats.primaryNumber
+
 
 document.getElementById("name").innerHTML = name
 document.getElementById("avg").innerHTML = avg
@@ -106,5 +107,37 @@ document.getElementById("slg").innerHTML = slg
 document.getElementById("rbis").innerHTML = rbis
 document.getElementById("hrs").innerHTML = hrs
 document.getElementById("ops").innerHTML = ops
+document.getElementById("header").innerHTML = '#' + number + ' ' + name + '<br></br>' + position + ' - ' + team
+
+
 // export {get2023PlayerStats};
 
+async function getTeam() {
+    let response = await fetch("https://site.api.espn.com/apis/site/v2/sports/baseball/mlb/teams/1?enable=roster");
+    let data = await response.json()
+    return data
+};
+
+let orioles = await getTeam()
+
+
+
+// Establishes variable to hold link to player's headshot
+var playerPic = ''
+
+async function getHeadshot() {
+    // Goes through list of team's players and finds the headshot for the player whose name matches the input
+    for (let i = 0; i < orioles.team.athletes.length; i++) {
+        if (orioles.team.athletes[i].fullName == inputPlayer) {
+            playerPic = orioles.team.athletes[i].headshot.href
+            break
+        }
+    }
+    return playerPic
+};
+
+// Calls function that returns link to player's headshot
+let headshot = await getHeadshot()
+document.getElementById("headshot").src = headshot
+
+console.log('player pic', playerPic)
